da77cb262efe33c6eb54d7c4deb604fb6836fe73
Max Isom <codetheweb@icloud.com>
2016-09-20 14:01:25 -0500

    If at first you don't succeed, try, try, again

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+#!/usr/local/env python
 import requests
 import base64
 import os.path
+import time
 
-filename = "data.csv" # where the data will be saved
+filename = "/Users/maxisom/Documents/SYNC/School/2016-2017/Max/Economics/data.csv" # where the data will be saved
 
 Symbols = ["DJIA", "USDX", "OIL", "SILVER", "GOLD"] # symbols that should be recorded
 
 def getData(symbol): # gets specified symbol using Kitco's API
-	r = requests.get('http://charts.kitco.com/KitcoCharts/RequestHandler?requestName=getSymbolSnapshot&Symbol=' + symbol)
-	return base64.b64decode(r.content).split(',')
+	tries = 0
+	while tries != 100: # if it doesn't work the first time, it's likely a faulty internet connection
+		try:
+			r = requests.get('http://charts.kitco.com/KitcoCharts/RequestHandler?requestName=getSymbolSnapshot&Symbol=' + symbol)
+			return base64.b64decode(r.content).split(',')
+		except:
+			print 'Error!'
+			time.sleep(2) # delay for 2 seconds, then try again
+
+		tries = tries + 1
+
+	return False # if we can't connect after 100 tries return false
 
 def getBid(symbol):
-	return getData(symbol)[1]
+	if (getData(symbol) != False):
+		return getData(symbol)[1]
+	return False
 
 def getBidTime(symbol):
-	return getData(symbol)[0]
+	if (getData(symbol) != False):
+		return getData(symbol)[0]
+	return False
 
 # for some reason opening with open(file, 'a') doesn't work even though it should
 if (os.path.isfile(filename)):
@@ -25,6 +41,9 @@ else:
 if (file.read() == ""): # write out header if it doesn't exist
 	file.write('Date,' + ','.join(Symbols) + '\n')
 
+if (getBid(Symbols[0]) == False): # exit if we can't connect
+	exit()
+	
 newDataRow = getBidTime(Symbols[0]) + ', ' # start off new data row with the last updated time
 
 for symbol in Symbols: # loops over the specified symbols and saves the bid price of each one
